7bd9ebdd30cda2f39ecb0ac095a5a708d519c906
ndaley <ndaley@L11712038606.wctc.msft>
2013-10-23 19:13:29 -0500

    Lab 7 complete. Also removed unneeded import in Lab 6.

--- src/class17lab/Lab7.java
+++ src/class17lab/Lab7.java
@@ -2,10 +2,10 @@ package class17lab;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Set;
 
 /**
  *
@@ -19,34 +19,28 @@ public class Lab7 {
         Employee emp3 = new Employee("Bond", "James", "007-07-0007");
         Employee emp4 = new Employee("Love", "Kim", "123-12-1234");
         
-        Map<String, Employee> employeeMap = new HashMap<String, Employee>();
+        Set<Employee> employeeSet = new HashSet<Employee>();
         
-        employeeMap.put(emp1.getSsn(), emp1);
-        employeeMap.put(emp2.getSsn(), emp2);
-        employeeMap.put(emp3.getSsn(), emp3);
-        employeeMap.put(emp4.getSsn(), emp4);
-        
-        System.out.println("Employee map size: " + employeeMap.size());
-        Employee copy = (Employee)employeeMap.get("007-07-0007");
+        employeeSet.add(emp1);
+        employeeSet.add(emp2);
+        employeeSet.add(emp3);
+        employeeSet.add(emp4);
         
         System.out.println("");
-        System.out.println("Employee at with ssn 007-07-0007: ");
-        System.out.println(copy.toString());
+        System.out.println("Entire set output with for each(set is sorted by SSN): ");
         
-        System.out.println("");
-        System.out.println("Entire map output with for each(map is sorted by SSN): ");
-        
-        for(String s : employeeMap.keySet()){
-            System.out.println(employeeMap.get(s).toString());
+        for(Iterator itr = employeeSet.iterator(); itr.hasNext();){
+            System.out.println(itr.next().toString());
         }
         
-        List<Employee> employeeList = new ArrayList<Employee>(employeeMap.values());
+        List<Employee> employeeList = new ArrayList<Employee>(employeeSet);
         Collections.sort(employeeList, new EmployeeLastNameSorter());
         
         System.out.println("");
-        System.out.println("Sorted list from map values, sorted with a Comparator(sorted by last name):");
+        System.out.println("Sorted list from set, sorted with a Comparator(sorted by last name):");
         for(Employee e : employeeList){
             System.out.println(e.toString());
         }
+        
     }
 }
